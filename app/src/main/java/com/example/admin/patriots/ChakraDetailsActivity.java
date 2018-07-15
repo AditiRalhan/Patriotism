04b31683e1c1f5b29283f646d220b7d889bdcc2e
Himanshu <60himanshu17@quicfind.com>
2018-07-15 11:12:52 +0530

    Fixes in awards section, fixed fonts issue

--- app/src/main/java/com/example/admin/patriots/ChakraDetailsActivity.java
+++ app/src/main/java/com/example/admin/patriots/ChakraDetailsActivity.java
@@ -43,20 +43,22 @@ public class ChakraDetailsActivity extends AppCompatActivity implements GridRecy
                     recipientsList.add(new NationalistData("Karam Singh","https://upload.wikimedia.org/wikipedia/en/thumb/3/3c/Karam_Singh.jpg/220px-Karam_Singh.jpg","https://en.wikipedia.org/wiki/Karam_Singh"));
                     recipientsList.add(new NationalistData("Gurbachan Singh Salaria","https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Captain_G_S_Salaria.jpg/220px-Captain_G_S_Salaria.jpg","https://en.wikipedia.org/wiki/Gurbachan_Singh_Salaria"));
                     recipientsList.add(new NationalistData("Dhan Singh Thapa","https://upload.wikimedia.org/wikipedia/en/thumb/4/48/Dhan_Singh_Thapa.jpg/250px-Dhan_Singh_Thapa.jpg","https://en.wikipedia.org/wiki/Dhan_Singh_Thapa"));
-                    recipientsList.add(new NationalistData("Joginder Singh","https://upload.wikimedia.org/wikipedia/en/thumb/2/2b/Joginder_SIngh_PVC.jpg/250px-Joginder_SIngh_PVC.jpg","https://en.wikipedia.org/wiki/Joginder_Singh_(soldier"));
+                    recipientsList.add(new NationalistData("Joginder Singh","https://upload.wikimedia.org/wikipedia/en/thumb/2/2b/Joginder_SIngh_PVC.jpg/250px-Joginder_SIngh_PVC.jpg","https://en.wikipedia.org/wiki/Joginder_Singh_(soldier)"));
                     recipientsList.add(new NationalistData("Shaitan Singh","https://upload.wikimedia.org/wikipedia/en/thumb/c/c4/Shaitan_Singh_PVC.jpg/200px-Shaitan_Singh_PVC.jpg","https://en.wikipedia.org/wiki/Shaitan_Singh"));
                     recipientsList.add(new NationalistData("Abdul Hamid","https://upload.wikimedia.org/wikipedia/en/thumb/1/1a/Abdul_Hamid_PVC.jpg/250px-Abdul_Hamid_PVC.jpg","https://en.wikipedia.org/wiki/Abdul_Hamid_(soldier)"));
                     recipientsList.add(new NationalistData("Ardeshir Tarapore","https://upload.wikimedia.org/wikipedia/en/3/3a/Ardeshir_Tarapore_PVC.jpg","https://en.wikipedia.org/wiki/Ardeshir_Tarapore"));
                     recipientsList.add(new NationalistData("Albert Ekka","https://upload.wikimedia.org/wikipedia/en/e/eb/Albert_Ekka_PVC.jpg","https://en.wikipedia.org/wiki/Albert_Ekka"));
-                    recipientsList.add(new NationalistData("Nirmal Jit Singh Sekhon","https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Statue_of_Nirmal_Jit_Singh_Sekhon_and_his_aircraft%2C_10_sep_2013.jpg/220px-Statue_of_Nirmal_Jit_Singh_Sekhon_and_his_aircraft%2C_10_sep_2013.jpg","https://en.wikipedia.org/wiki/Nirmal_Jit_Singh_Sekhon"));
+                    recipientsList.add(new NationalistData("Nirmal Jit Singh Sekhon","http://www.defenceacademy.in/wp-content/uploads/2017/08/nirmaljitsinghsekhononlyindianairforceofficertowinparamvirchakra652x4001_1439989237-278x365.jpg","https://en.wikipedia.org/wiki/Nirmal_Jit_Singh_Sekhon"));
                     recipientsList.add(new NationalistData("Arun Khetarpal","https://upload.wikimedia.org/wikipedia/en/2/29/Arun_Khetarpal_PVC.jpg","https://en.wikipedia.org/wiki/Arun_Khetarpal"));
                     recipientsList.add(new NationalistData("Hoshiar Singh","https://upload.wikimedia.org/wikipedia/en/c/c9/Hoshiar_Singh_PVC.jpg","https://en.wikipedia.org/wiki/Hoshiar_Singh"));
-                    recipientsList.add(new NationalistData("Bana Singh","","https://en.wikipedia.org/wiki/Bana_Singhhttps://upload.wikimedia.org/wikipedia/commons/2/2a/Bana_Singh_PVC.jpg"));
-                    recipientsList.add(new NationalistData("Ramaswamy Parameshwaran","","https://en.wikipedia.org/wiki/Ramaswamy_Parameshwaran"));
+                    recipientsList.add(new NationalistData("Bana Singh","http://gallantryawards.gov.in/sites/default/files/styles/awardee_img/public/sites/default/files/66_1.jpg?itok=r8SpYXsz","https://en.wikipedia.org/wiki/Bana_Singh"));
+                    recipientsList.add(new NationalistData("Ramaswamy Parameshwaran","http://gallantryawards.gov.in/sites/default/files/styles/awardee_img/public/sites/default/files/36_2.jpg?itok=q0JaLgg1","https://en.wikipedia.org/wiki/Ramaswamy_Parameshwaran"));
                     recipientsList.add(new NationalistData("Yogendra Singh Yadav","https://upload.wikimedia.org/wikipedia/commons/0/01/Yogendra_Singh_Yadav_PVC.jpg","https://en.wikipedia.org/wiki/Yogendra_Singh_Yadav"));
                     recipientsList.add(new NationalistData("Manoj Kumar Pandey","https://upload.wikimedia.org/wikipedia/en/6/64/Manoj_Kumar_Pandey_PVC.jpg","https://en.wikipedia.org/wiki/Manoj_Kumar_Pandey\t"));
                     recipientsList.add(new NationalistData("Sanjay Kumar","https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Sanjay_Kumar_PVC.jpg/220px-Sanjay_Kumar_PVC.jpg","https://en.wikipedia.org/wiki/Sanjay_Kumar_(soldier)"));
                     recipientsList.add(new NationalistData("Vikram Batra","https://upload.wikimedia.org/wikipedia/en/5/56/Vikram_Batra_PVC.jpg","https://en.wikipedia.org/wiki/Vikram_Batra"));
+
+
 
                     recyclerView=findViewById(R.id.chakra_details_recycler_view_id);
                     recyclerView.setLayoutManager(new GridLayoutManager(this,2));
@@ -80,7 +82,7 @@ public class ChakraDetailsActivity extends AppCompatActivity implements GridRecy
                     recipientsList.add(new NationalistData("Rakesh Sharma","https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Rakesh_sharma.jpg/220px-Rakesh_sharma.jpg","https://en.wikipedia.org/wiki/Rakesh_Sharma"));
                     recipientsList.add(new NationalistData("Gennadi Strekalov","https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Gennady_Strekalov.jpg/220px-Gennady_Strekalov.jpg","https://en.wikipedia.org/wiki/Gennadi_Strekalov"));
                     recipientsList.add(new NationalistData("Yury Malyshev","https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/1980_CPA_5108.jpg/220px-1980_CPA_5108.jpg","https://en.wikipedia.org/wiki/Yury_Malyshev_(cosmonaut)"));
-                    recipientsList.add(new NationalistData("Bachittar Singh","https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Fort_of_Kotla_Nihang_Khan%2C_village_Kotla_Nihang_Khan%2C_Rupnagar_district%2CPunjab%2CIndia.jpg/220px-Fort_of_Kotla_Nihang_Khan%2C_village_Kotla_Nihang_Khan%2C_Rupnagar_district%2CPunjab%2CIndia.jpg","https://en.wikipedia.org/wiki/Bachittar_Singh"));
+                    recipientsList.add(new NationalistData("Bachittar Singh","http://gallantryawards.gov.in/sites/default/files/styles/awardee_img/public/sites/default/files/1_2.jpg?itok=6Dxtp4J3","https://www.honourpoint.in/profile/havaldar-bachittar-singh-ac/"));
 
                     recyclerView=findViewById(R.id.chakra_details_recycler_view_id);
                     recyclerView.setLayoutManager(new GridLayoutManager(this,2));
